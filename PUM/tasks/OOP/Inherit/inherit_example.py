b97a236794666500f740fdead8fa415b9c6699c9
Bagiyan Artem <bagiyanartem@yandex.ru>
2020-10-04 17:57:02 +0300

    update

--- PUM/tasks/OOP/Inherit/inherit_example.py
+++ PUM/tasks/OOP/Inherit/inherit_example.py
@@ -4,50 +4,65 @@ class Pet:
         self.name = name
 
     def say(self):
-        return "{0}: -----".format(self.name)
+        return self.name
 
-
+# определили класс Dog как наследника класса Pet
 class Dog(Pet):
 
     def __init__(self, name, breed):
+        # super() - обращение к классу предка, в данном случае - класс Pet
+        # таким образом заполняем атрибут name с помощью уже написанного метода __init__ у класса Pet
         super().__init__(name)
         self.breed = breed
 
     def say(self):
-        return "{0}: waw".format(self.name)
+        return super().say() + " : waw"
 
+# определили класс FightingDog как наследника класса Dog
 class FightingDog(Dog):
 
     def __init__(self, name, breed, is_angry):
+        # использование __init__ класса Dog
         super().__init__(name, breed)
-        self.is_angry = is_angry
-
-    def say(self):
-        return super().say() + 30*'waw' if self.is_angry else super().say()
+        self.__is_angry = is_angry
 
     def feed(self):
-        self.is_angry = False
-    
-    def step_on_tail(self):
-        self.is_angry = True
+        self.__is_angry = False
 
+    def step_on_tail(self):
+        self.__is_angry = True
+
+    def say(self):
+        if self.__is_angry:
+            # super() можно использовать также и для других методов класса предка
+            # в данном случае используем уже написанный метод say() класса Dog
+            return super().say() + 10*"waw"
+        else:
+            return super().say()
+
+# определили класс Crocodile как наследника класса Pet
 class Crocodile(Pet):
-    
+
     def __init__(self, name, weight):
+        # использование __init__ класса Pet
         super().__init__(name)
         self.weight = weight
 
-        
-    
-dog = FightingDog("Кузя", "Овчарка", False)
+    def say(self):
+        return super().say() + " : rrrrr"
 
-# for i in range(10):
-#     if i % 2 == 1:
-#         dog.step_on_tail()
-#     else:
-#         dog.feed()
+    # метод say переопределяется у всех потомков класса Pet
+    # вызов метода выглядит одинаковым для объектов всех созданных нами классов,
+    # однако исполняемый код разный (полиморфизм)
 
-#     print(dog.say())
 
-croc = Crocodile("Боря", 73)
-print(croc.say())
+# Работа с созданной иерархией
+
+a = FightingDog("Мухтар", "Овчарка", False)
+b = Dog("Тузик", "Мопс")
+с = Crocodile("Ник", 100)
+
+zoo = [a, b, c]
+
+for animal in zoo:
+    print(animal.say())
